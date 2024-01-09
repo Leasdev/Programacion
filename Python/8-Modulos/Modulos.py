13befedca8434ddbfa8bbfd21096bfbf03a26e7d
Leachimi <sunleandro010@gmail.com>
2024-01-09 18:40:29 -0300

    9/1

---
+++ Python/8-Modulos/Modulos.py
@@ -0,0 +1,13 @@
+# Modulos
+"""import aritmeticas"""  # namespace
+# print(aritmeticas.sumar(1,2))
+
+"""import aritmeticas as funciones""" # cambia el namespace
+# print(funciones.restar(1,2))
+
+"""from aritmeticas import restar""" # importa la funcion especifica
+#print(restar(1,2))
+
+
+
+
---
+++ Python/8-Modulos/aritmeticas.py
@@ -0,0 +1,16 @@
+def sumar(x,y):
+    return x + y
+
+def restar(x,y):
+    return x - y
+
+def multiplicar(x,y):
+    return x * y
+
+def factorial(x):
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return x * factorial(x-1)
+    
+    
