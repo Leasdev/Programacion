d52e7873f78fd0ab1a73880a1409d9a217517ba9
Leachimi <sunleandro010@gmail.com>
2024-01-10 19:06:40 -0300

    10/1 v3

---
+++ C++/Imperativo/2-Asignacion/Interna.cpp
@@ -0,0 +1,22 @@
+// Asignacion Interna
+
+/*
+   indentificador = expresion; 
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+
+    y = 10;
+    x = 25 * y + 5;
+
+    cout << "y = 10" << endl;
+    cout << "x = 25 * y + 5" << endl;
+    cout << "x = " << x << endl;
+
+    return 0;
+}
