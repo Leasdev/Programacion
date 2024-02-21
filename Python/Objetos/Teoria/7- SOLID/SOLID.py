352d3ad3cc57f48817e195c15e2dd7f7dbc175dd
Leachimi <sunleandro010@gmail.com>
2024-02-21 17:04:23 -0300

    Wed, Feb 21, 2024  5:04:23 PM

---
+++ Python/Objetos/Teoria/7- SOLID/SOLID.py
@@ -0,0 +1,23 @@
+# SOLID
+"""
+S: SRP PRINCIPIO DE RESPONSABILIDAD UNICO
+O: OCP PRINCIPIO DE ABIERTO/CERRADO
+L: LSP PRINCIPIO DE SUSTITUCION DE LISKOV
+I: ISP PRINCIPIO DE SEGREGACION DE LA INTERFAZ
+D: DIP PRINCIPIO DE INVERSION DE DEPENDENCIAS
+"""
+
+# SRP 
+# cada clase tiene una unica funcion para hacer
+
+# OCP
+# las entidades de software tiene que estar abierto para la extension y cerrada para la modificacion
+
+# LSP
+# Las clases derivadas tiene que ser sustituible por sus clase base
+
+# ISP
+# ningun cliente dependa de un interfaz que utilicemos
+
+# DIP
+# las dependencias de alto nivel no depende de las implementaciones especificas 
